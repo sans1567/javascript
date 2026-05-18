9621d7ef80c5f0e8002bdb250ad4281524005a73
Asato <dmat.1047sxx@gmail.com>
2026-05-18 15:08:57 +0900

    feature/animationの編集

---
+++ script.js
@@ -0,0 +1,34 @@
+const hambragerBtn = document.querySelector('.hambrager-btn');
+const headerMenu = document.querySelector('.header__menu');
+const closeBtn = document.querySelector('.header__btn--close');
+
+console.log(hambragerBtn,headerMenu,closeBtn);
+
+function hambrgerActive() {
+    hambragerBtn.classList.toggle('is-active');
+    headerMenu.classList.toggle('is-show');
+};
+
+function headerMenuClose() {
+    hambragerBtn.classList.remove('is-active');
+    headerMenu.classList.remove('is-show');
+};
+
+hambragerBtn.addEventListener('click', () => {
+    hambrgerActive();
+});
+
+closeBtn.addEventListener('click', () => {
+    headerMenuClose();
+});
+
+/*ハンバーガーボタン要素かつハンバーガーメニュー要素が含まれていないか*/
+document.body.addEventListener('click', (event) => {
+    if(
+        !hambragerBtn.contains(event.target) && 
+        !headerMenu.contains(event.target)
+    ){
+        hambragerBtn.classList.remove('is-active');
+        headerMenu.classList.remove('is-show');
+    }
+});
